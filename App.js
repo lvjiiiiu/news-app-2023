8d471f5e97a4adcb4770844e3c840bfa5873193b
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-03-21 16:37:25 +0900

    セクション3-16

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View, Text } from 'react-native';
+import { ListItem } from './components/ListItem'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image 
-            style={{width: 100, height: 100}}
-            source={{uri: "https://picsum.photos/200"}}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            hoge hoge hoge hoge hoge hoge
-          </Text>
-          <Text style={styles.subText}>
-            ReactNews
-          </Text>
-        </View>
-      </View>
+      <ListItem />
+      <ListItem />
+      <ListItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -31,26 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    backgroundColor: 'white',
-    width: "100%",
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between"
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
-  },
+  }
 });
