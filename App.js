b5e84f8d442f99a0abe0a69787fb9473e5cafd6d
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-03-21 17:01:09 +0900

    セクション4-21

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View, Text } from 'react-native';
 import { ListItem } from './components/ListItem'
+import articles from './dummies/articles'
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem imageUrl={article.urlToImage} title={article.title} author={article.author} key={index.toString()}/>
+    )
+  })
   return (
     <View style={styles.container}>
-      <ListItem imageUrl={"https://picsum.photos/200"} title={"hoge hoge hoge"} author={"React News"}/>
-      <ListItem />
-      <ListItem />
+      {items}
       <StatusBar style="auto" />
     </View>
   );
