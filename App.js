cc9230f33c3845af1433c7a35ce7e9285dbde3cf
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-03-21 16:24:53 +0900

    セクション3-14

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}></View>
+        <View style={styles.leftContainer}>
+          <Image 
+            style={{width: 100, height: 100}}
+            source={{uri: "https://picsum.photos/200"}}
+          />
+        </View>
         <View style={styles.rightContainer}></View>
       </View>
       <StatusBar style="auto" />
