30280f0cd5bafdbd7343b80ccbf8e5b62c0f0144
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-03-21 17:59:58 +0900

    セクション6-32

---
+++ screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { ListItem } from '../components/ListItem'
+import axios from 'axios'
+import Constants from 'expo-constants';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export const HomeScreen = () => {
+  const [articles, setArticles] = useState()
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL)
+      setArticles(response.data.articles)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({item}) =>(<ListItem imageUrl={item.urlToImage} title={item.title} author={item.author}/>)}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+  }
+});
